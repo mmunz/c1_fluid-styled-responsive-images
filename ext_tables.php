9f43f87850edd71577d90dafadc8408b01fd0245
Manuel Munz <munz@comuno.net>
2015-12-29 14:59:01 +0100

    Add DataProcessor, moved from c1_fsc_image

--- ext_tables.php
+++ ext_tables.php
@@ -6,3 +6,7 @@ defined('TYPO3_MODE') or die();
     'Configuration/TypoScript',
     'Fluid Styled Responsive Images'
 );
+
+// Add TCEFORM Config
+$tceformConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TSConfig/tceform.ts');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($tceformConfig);
